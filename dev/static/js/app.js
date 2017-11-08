46adc5056813c0eee2893b1c715cd1fce4c9933f
Лейкин Андрей Романович <Leykin.AR@kamaz.org>
2017-11-08 14:38:08 +0300

    start park

---
+++ dev/static/js/app.js
@@ -0,0 +1,38 @@
+// Начальная функция
+
+(function(){
+	var footerItem = $('.footer__item'),
+		aboutItem = $('.about__item'),
+		aboutPart = $('.about__part'),
+		footerActive = 'footer__item--active',
+		aboutItemActive = 'about__item--active',
+		aboutPartAnimate = 'about__part--animate';
+
+	footerItem.first().addClass(footerActive);
+	aboutItem.first().addClass(aboutItemActive);
+
+	setTimeout(function(){
+		aboutPart.addClass(aboutPartAnimate);
+	},500);
+
+	footerItem.click(function() {
+
+		var $this = $(this),
+			index = $this.index(),
+			reqFooter = footerItem.eq(index),
+			reqItem = aboutItem.eq(index);
+
+		if($this.data('color') == 'green') {
+			$this.closest('.wrapper').removeClass('wrapper--blue').addClass('wrapper--green');
+		} else {
+			$this.closest('.wrapper').removeClass('wrapper--green').addClass('wrapper--blue');
+		}
+
+			if($this.hasClass(footerActive)) {
+				return false;
+			} else {
+				reqFooter.addClass(footerActive).siblings().removeClass(footerActive);
+				reqItem.addClass(aboutItemActive).siblings().removeClass(aboutItemActive);
+			}
+	});
+})();
